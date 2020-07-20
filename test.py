76070bd22158ebad17f9b54c8f0fa660538d07d9
Mandy Hulst <mandyhulst@gmail.com>
2020-07-20 18:24:13 +0200

    first upload

---
+++ test.py
@@ -0,0 +1,7 @@
+# with open('data.txt', 'r') as file:  # if data is given as graph
+#     graph = dict((line.strip().split(' -> ') for line in file))
+# for key in graph:
+#     graph[key] = graph[key].split(',')
+# graph = {int(k): [int(i) for i in v] for k, v in graph.items()}
+
+
